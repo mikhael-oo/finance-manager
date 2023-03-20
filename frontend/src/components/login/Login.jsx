afc847104c0ff735468b2eb69ef6d0f27713f551
mao4 <mao4@sfu.ca>
2023-03-19 19:03:24 -0700

    Basic login/ signup form

---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,53 @@
+import { React, useState } from 'react';
+
+
+function Login(props) {
+
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("submitted")
+        console.log(user)
+        console.log(pass)
+    }
+
+    const onSuccess = (res) => {
+        console.log("LOGIN SUCCESSFUL!!! USER: ", res.profileObj);
+    }
+
+    const onFailure = (res) => {
+        console.log("LOGIN UNSUCCESSFUL!!! res: ", res);
+    }
+
+    return(
+        <div className="auth-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label>Username/email:</label>
+                    <input
+                        name="username"
+                        type="text"
+                        id="username"
+                        value={user}
+                        onChange={(e) => setUser(e.target.value)}
+                    />
+                
+                <label>Password:</label> 
+                    <input
+                        name="password"
+                        type="password"
+                        id="password"
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}
+                    />     
+                <input type="submit" />
+            </form>
+
+            <button className="link-btn" onClick={ () => props.onFormSwitch('SignUp')}>Don't Have an Account? Register Here</button>
+
+        </div>
+    )
+}
+
+export default Login;
