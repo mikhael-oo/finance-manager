4c3ace83ae4dfeb7ba5b9458cff1be01d5a3cea9
Austin Kwan <aka120@sfu.ca>
2023-03-22 17:01:12 -0700

    basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,7 @@ import './App.css';
 import Login from "./components/login/Login"
 import SignUp from "./components/signup/SignUp"
 
-
-
-
-
-
 function App() {
-
-
 
   const [currentForm, setCurrentForm] = useState('Login')
   
