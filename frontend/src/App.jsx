404fe60b5af800e75cabd4f23e4db46f8831f7af
Austin Kwan <aka120@sfu.ca>
2023-03-23 01:36:25 -0700

    moved router from main to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Login from "./components/login/Login"
 import SignUp from "./components/signup/SignUp"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Account } from './components/pages/Account'
+import { Bill } from './components/pages/Bill'
+import { Expense } from './components/pages/Expense'
+import { Home } from './components/pages/Home'
+import Layout from './components/pages/Layout'
+import { Planning } from './components/pages/Planning'
+import { AppProvider } from './context/AppContext'
 
 function App() {
 
@@ -13,9 +21,28 @@ function App() {
 
   return (
     <div className="App">
-      {
-        currentForm === "Login" ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>
-      }
+      <AppProvider>
+        <BrowserRouter>
+        <Routes>
+          <Route index element={currentForm === "Login" ? <Login onFormSwitch={toggleForm}/> : <SignUp onFormSwitch={toggleForm}/>} />
+          <Route path='/home' element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path='/expense' element={<Layout />}>
+            <Route index element={<Expense />} />
+          </Route>
+          <Route path='/bill' element={<Layout />}>
+            <Route index element={<Bill />} />
+          </Route>
+          <Route path='/planning' element={<Layout />}>
+            <Route index element={<Planning />} />
+          </Route>
+          <Route path='/account' element={<Layout />}>
+            <Route index element={<Account />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      </AppProvider>
       
     </div>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,39 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-import { Account } from './components/pages/Account'
-import { Bill } from './components/pages/Bill'
-import { Expense } from './components/pages/Expense'
-import { Home } from './components/pages/Home'
-import Layout from './components/pages/Layout'
-import { Planning } from './components/pages/Planning'
 import './index.css'
-import { AppProvider } from './context/AppContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<App />} />
-          <Route path="/home" element={<Layout />}>
-            <Route index element={<Home />} />
-          </Route>
-          <Route path="/expense" element={<Layout />}>
-            <Route index element={<Expense />} />
-          </Route>
-          <Route path="/bill" element={<Layout />}>
-            <Route index element={<Bill />} />
-          </Route>
-          <Route path="/planning" element={<Layout />}>
-            <Route index element={<Planning />} />
-          </Route>
-          <Route path="/account" element={<Layout />}>
-            <Route index element={<Account />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AppProvider>
+    <App />
   </React.StrictMode>,
-)
+);
