f9f3de6147d19d108aa1c71be92d5cf5b1ae6b8d
mao4 <mao4@sfu.ca>
2023-04-11 22:14:32 -0700

    final changes

---
+++ frontend/src/components/expenses/ExpenseItem.jsx
@@ -0,0 +1,14 @@
+// import {React} from 'react';
+
+// const ExpenseItem = (props) => {
+
+//     return (
+//         // <li>
+//         //     Expense: {props.name} Amount: {props.amount} Category: {props.category} Date: {props.date}
+//         // </li>
+        
+//     );
+// };
+
+
+// export default ExpenseItem;
---
+++ frontend/src/components/pages/Planning.jsx
@@ -0,0 +1,14 @@
+import AddBudget from '../budget/AddBudget';
+import Remaining from '../remaining/Remaining';
+import Budget from '../budget/Budget';
+
+export const Planning = () => {
+    return (
+        <div>
+            <p>Planning</p>
+            <AddBudget />
+            <Budget />
+            <Remaining />
+        </div>
+    );
+};
